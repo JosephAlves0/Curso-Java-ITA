5e57c8bf3f5ee838e33b7556fcc2a44ab5569dea
JosephAlves0 <j.eduardo.alves@hotmail.com>
2025-01-03 14:41:35 -0300

    Finalizando projeto Carro2

--- Carro2/src/main/java/Principal.java
+++ Carro2/src/main/java/Principal.java
@@ -1,5 +1,13 @@
 
 public class Principal {
+    public static void main(String[] args) {
+        Corrida corridaDaAmizade = new Corrida(2000);
+        corridaDaAmizade.adicionaCarro(new CarroSoma(10, 110, "Carro A"));
+        corridaDaAmizade.adicionaCarro(new CarroSoma(8,110,"Carro B"));
+        corridaDaAmizade.adicionaCarro(new CarroMult(100,1.7,"Carro C"));
+        corridaDaAmizade.adicionaCarro(new CarroMult(110,1.4,"Carro D"));
 
+        corridaDaAmizade.umDoisTresEJa();
+    }
 }
 
